884111f1f53823e22ea2bd3faa5dc312ca440e23
nguyen1207 <hoangnguyen789a@gmail.com>
2021-07-05 10:40:37 +0700

    UI and background change color base on location temperature

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
   $("#get-weather-form").submit(fetchWeatherInfo);
-  
+  $("input")
+    .focus(function() {
+      $(this).attr("placeHolder", "");
+    })
+    .blur(function() {
+      $(this).attr("placeHolder", "Your location");
+    })
+    .click(function() {
+      $(this).select()
+    });
+
   function fetchWeatherInfo(e) {
     e.preventDefault();
     const API_KEY = "e3317d99839f28cae3e45874131d91f2";
     const CITY_NAME = $("#city-name-input").val();
-    console.log(CITY_NAME)
     const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME.replace(/\s/g, "").trim()}&appid=${API_KEY}&units=metric`
 
     fetch(URL)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
 
         // If not found city
         if(data.message) {
@@ -33,16 +41,30 @@ $(document).ready(function () {
         let weatherIconId = data.weather[0].icon;
         let weatherIconImg = `http://openweathermap.org/img/wn/${weatherIconId}@2x.png`
         let humidity = data.main.humidity;
-        console.log(cityName, temperature, weather, humidity)
 
         $("#city-name").html(`<span>${cityName}</span>`);
-        $("#weather").html(`<img src="${weatherIconImg}"><span>${weather}</span>`);
-        $("#temperature").html(`<span>${temperature}</span>`);
-        $("#humidity").html(`<span>${humidity}</span>`);
+        $("#weather").html(`<div id="weather-icon"><img src="${weatherIconImg}"></div><div id="weather-description"><span>${weather}</span></div>`);
+        $("#temperature").html(`<i class="fas fa-thermometer-half"></i><span>${temperature}°C</span>`);
+        $("#humidity").html(`<i class="fas fa-tint"></i><span>${humidity}%</span>`);
+        $("input").val("").blur();
+
+        changeBackgroundColorByTemperature(temperature);
       }) 
   }
   
   function uppercaseFirstLetter(string) {
     return string.replace(string[0], string[0].toUpperCase());
+  }
+
+  function changeBackgroundColorByTemperature(temperature) {
+    if(temperature > 35) changeBackgroundColor("rgba(253,83,6,255)");
+    else if(temperature > 27) changeBackgroundColor("rgba(249,188,1,255)");
+    else if(temperature > 20) changeBackgroundColor("rgba(104,176,51,255)")
+    else if(temperature > 10) changeBackgroundColor("rgba(104,176,51,255)");
+    else changeBackgroundColor("rgba(60,2,166,255)");
+  }
+
+  function changeBackgroundColor(color) {
+    $("html").css({backgroundColor: color});
   }
 });
